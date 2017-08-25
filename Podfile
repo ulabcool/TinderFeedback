4ae29302fd16572721d6976c923c135524c3d4fd
Adil Bougamza <adil.bougamza@gmail.com>
2017-08-25 10:08:29 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TinderFeedback' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Koloda', '~> 4.0'
+
+end
---
+++ TinderFeedback/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderFeedback
+//
+//  Created by Adil Bougamza on 25/08/2017.
+//  Copyright © 2017 Adil Bougamza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
